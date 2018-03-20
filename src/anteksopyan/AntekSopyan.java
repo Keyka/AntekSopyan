e9048d501de6bee5748c3894134182f6b59f730e
Dwi Wahyu <bxmailo@gmail.com>
2018-03-20 20:49:28 +0700

    hoohoh

--- src/anteksopyan/AntekSopyan.java
+++ src/anteksopyan/AntekSopyan.java
@@ -15,7 +15,8 @@ public class AntekSopyan {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic here hoho
+        
     }
     
 }
